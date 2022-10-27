88f4193a584ca3d971996dc33e9d3cec30bac4a9
JinHyeon Hong <kerriganhjh@naver.com>
2022-10-27 19:16:05 +0900

    test 111

--- contracts/eden/tests/test-eden.cpp
+++ contracts/eden/tests/test-eden.cpp
@@ -976,8 +976,12 @@ TEST_CASE("budget distribution")
    //     {"alice"_n, s2a("1.8000 EOS")}};
    // CHECK(t.get_budgets_by_period_nameandasset() == expected);
 
-   expect(t.alice.trace<actions::fundtransfer>("alice"_n, s2t("2020-04-04T15:30:00.000"), 1,
-                                               "egeon"_n, s2a("1.8001 EOS"), "memo"),
+    t.alice.act<actions::fundtransfer>("alice"_n, s2t("2020-04-04T15:30:00.000"), 1, "egeon"_n,
+                                      s2a("1.8000 EOS"), "memo");
+   CHECK(get_eden_account("egeon"_n)->balance() == s2a("1.8000 EOS"));
+
+   // expect(t.alice.trace<actions::fundtransfer>("alice"_n, s2t("2020-04-04T15:30:00.000"), 1,
+   //                                             "egeon"_n, s2a("1.8001 EOS"), "memo"),
    //        "insufficient balance");
    // expect(t.alice.trace<actions::fundtransfer>("alice"_n, s2t("2020-04-04T15:30:00.000"), 1,
    //                                             "egeon"_n, s2a("-1.0000 EOS"), "memo"),
